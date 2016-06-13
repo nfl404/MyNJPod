13037d2752ced2ebfa1cabc819cd7fe989ed5305
niejia <niefuling@yonglibao.com>
2016-06-13 16:35:03 +0800

    update

---
+++ MyNJPod/MyNJPod/NJPods/NJPods.h
@@ -0,0 +1,13 @@
+//
+//  NJPods.h
+//  MyNJPod
+//
+//  Created by Mr nie on 16/6/13.
+//  Copyright © 2016年 程序猿:NiePlus   博客地址:http://nieplus.blog.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NJPods : NSObject
+
+@end
---
+++ MyNJPod/MyNJPod/NJPods/NJPods.m
@@ -0,0 +1,13 @@
+//
+//  NJPods.m
+//  MyNJPod
+//
+//  Created by Mr nie on 16/6/13.
+//  Copyright © 2016年 程序猿:NiePlus   博客地址:http://nieplus.blog.com. All rights reserved.
+//
+
+#import "NJPods.h"
+
+@implementation NJPods
+
+@end
